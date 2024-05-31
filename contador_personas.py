ef86d2d0b1c1fc02a02ce91a0951b6f1dfe3b760
Gio <gio.paule05@gmail.com>
2024-05-31 12:14:43 -0300

    commit 4

--- contador_personas.py
+++ contador_personas.py
@@ -6,16 +6,13 @@ import numpy as np
 import math
 from sort import Sort
 
-cap = cv2.VideoCapture("Videos/VideoAutos.mp4") #para video
+cap = cv2.VideoCapture("Videos/Video2.mp4") #para video
 cap.set(3, 1280)
 cap.set(4, 720)
 
 model = YOLO("yolov8n.pt")
 
 classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "potted plant", "bed", "dining table", "toilet", "tv monitor", "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
-
-#mascara para detectar solamente esa zona
-mask = cv2.imread("Imagenes/mask.png")
 
 #seguimiento/rastreador
 tracker = Sort(max_age=20, min_hits=3, iou_threshold=0.3)
@@ -26,9 +23,21 @@ totalCount = []
 
 while True:
     sucess, img = cap.read()
-    imgRegion = cv2.bitwise_and(img, mask)
     
-    results = model(imgRegion, stream=True)
+    imgGraphics = cv2.imread("Imagenes/graphics.png", cv2.IMREAD_UNCHANGED)
+    
+    # Verificar si la imagen tiene 3 canales (RGB) o 4 canales (RGBA)
+    if imgGraphics.shape[2] == 3:  # Si tiene 3 canales (RGB), agregar un canal alfa
+        b, g, r = cv2.split(imgGraphics)
+    # Crear un canal alfa completamente opaco (255) del mismo tamaño que la imagen
+        alpha = 255 * np.ones_like(b, dtype=b.dtype)
+    # Fusionar los canales RGB con el canal alfa para formar una imagen RGBA
+        imgGraphics = cv2.merge((b, g, r, alpha))
+    
+    imgGraphics = cv2.resize(imgGraphics, (0, 0), fx=0.5, fy=0.5)
+    
+    img = cvzone.overlayPNG(img, imgGraphics, (0, 0))
+    results = model(img, stream=True)
     
     detections = np.empty((0, 5))
     
@@ -50,7 +59,7 @@ while True:
             currentClass = classNames[cls]
             
             #solo muestra los dichos y si el nivel de confianza es bajo tampoco lo muestra
-            if currentClass == "car" or currentClass == "truck" or currentClass == "bus" or currentClass == "motorbike" and conf > 30:
+            if currentClass == "person" and conf > 30:
                 #mostrar un rectangulo con la confianza arriba de la caja sin pasarse del borde
                 #cvzone.putTextRect(img, f"{classNames[cls]} {conf:.2f}%", (max(0, x1), max(35, y1)), scale=1, thickness=1, offset=3) #scale = hacer el tamaño de la fuente mas chica, thickness = espesor, offset = tamaño del cuadro
                 #cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=3) #L = espacio entre los bordes, rt = espesor del rectangulo
@@ -78,7 +87,8 @@ while True:
                 totalCount.append(Id) #contar el numero por la id
                 cv2.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0, 255, 0), 3)
             
-    cvzone.putTextRect(img, f"Contador:{len(totalCount)}", (50, 50))
+    #cvzone.putTextRect(img, f"Contador:{len(totalCount)}", (50, 50))
+    cv2.putText(img, str(len(totalCount)), (255, 100), cv2.FONT_HERSHEY_PLAIN, 5, (0, 0, 0), 8)
             
     cv2.imshow("Imagenes", img)
     #cv2.imshow("ImageRegion", imgRegion)
