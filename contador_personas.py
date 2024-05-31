c40b258f496e63538e8f30eddfc72151d09e827a
Gio <gio.paule05@gmail.com>
2024-05-31 13:52:52 -0300

    commit 5

--- contador_personas.py
+++ contador_personas.py
@@ -6,7 +6,7 @@ import numpy as np
 import math
 from sort import Sort
 
-cap = cv2.VideoCapture("Videos/Video2.mp4") #para video
+cap = cv2.VideoCapture("Videos/people.mp4") #para video
 cap.set(3, 1280)
 cap.set(4, 720)
 
@@ -14,15 +14,22 @@ model = YOLO("yolov8n.pt")
 
 classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "potted plant", "bed", "dining table", "toilet", "tv monitor", "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
 
+#mascara para detectar solamente esa zona
+mask = cv2.imread("Imagenes/mask2.png")
+
 #seguimiento/rastreador
 tracker = Sort(max_age=20, min_hits=3, iou_threshold=0.3)
 
 #limites para crear una linea y cada vez que pasa de ese punto me cuenta el objeto
-limits = [450, 130, 950, 130]
-totalCount = []
+limitsUp = [103, 161, 296, 161]
+limitsDown = [527, 489, 735, 489]
+
+totalCountUp = []
+totalCountDown = []
 
 while True:
     sucess, img = cap.read()
+    imgRegion = cv2.bitwise_and(img, mask)
     
     imgGraphics = cv2.imread("Imagenes/graphics.png", cv2.IMREAD_UNCHANGED)
     
@@ -36,8 +43,8 @@ while True:
     
     imgGraphics = cv2.resize(imgGraphics, (0, 0), fx=0.5, fy=0.5)
     
-    img = cvzone.overlayPNG(img, imgGraphics, (0, 0))
-    results = model(img, stream=True)
+    img = cvzone.overlayPNG(img, imgGraphics, (730, 260))
+    results = model(imgRegion, stream=True)
     
     detections = np.empty((0, 5))
     
@@ -67,8 +74,8 @@ while True:
                 detections = np.vstack((detections, currentArray))
             
     resultsTracker = tracker.update(detections)
-    cv2.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0, 0, 255), 3) #,5 espesor
-    
+    cv2.line(img, (limitsUp[0], limitsUp[1]), (limitsUp[2], limitsUp[3]), (0, 0, 255), 3) #,5 espesor
+    cv2.line(img, (limitsDown[0], limitsDown[1]), (limitsDown[2], limitsDown[3]), (0, 0, 255), 3)
     #identificador
     for result in resultsTracker:
         x1, y1, x2, y2, Id = result
@@ -82,13 +89,13 @@ while True:
         cx, cy = x1+w//2, y1+h//2
         cv2.circle(img,(cx, cy), 5, (255,0,255), cv2.FILLED) #cuando el ciculo cae en la región, realiza el conteo
         
-        if limits[0] < cx < limits[2] and limits[1] - 20 < cy < limits[1] + 0:
-            if totalCount.count(Id) == 0:
-                totalCount.append(Id) #contar el numero por la id
-                cv2.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0, 255, 0), 3)
+    #     if limits[0] < cx < limits[2] and limits[1] - 20 < cy < limits[1] + 0:
+    #         if totalCount.count(Id) == 0:
+    #             totalCount.append(Id) #contar el numero por la id
+    #             cv2.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0, 255, 0), 3)
             
-    #cvzone.putTextRect(img, f"Contador:{len(totalCount)}", (50, 50))
-    cv2.putText(img, str(len(totalCount)), (255, 100), cv2.FONT_HERSHEY_PLAIN, 5, (0, 0, 0), 8)
+    # #cvzone.putTextRect(img, f"Contador:{len(totalCount)}", (50, 50))
+    # cv2.putText(img, str(len(totalCount)), (255, 100), cv2.FONT_HERSHEY_PLAIN, 5, (0, 0, 0), 8)
             
     cv2.imshow("Imagenes", img)
     #cv2.imshow("ImageRegion", imgRegion)
